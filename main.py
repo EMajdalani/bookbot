bd409da72162e3a91f3d4041e0c086ba06434d37
EMajdalani <EMajdalani121@gmail.com>
2025-03-20 23:37:35 -0400

    add comments to code

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#Reads the entire contents of the book and returns it as a string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
+#orchestrator function
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
